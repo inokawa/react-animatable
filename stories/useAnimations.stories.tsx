28c6b5f52f3bba8f136b00d5399e9ff9e1d5c057
inokawa <48897392+inokawa@users.noreply.github.com>
2022-06-18 21:07:49 +0900

    Create useAnimations.stories.tsx

---
+++ stories/useAnimations.stories.tsx
@@ -0,0 +1,47 @@
+import { StoryObj } from "@storybook/react";
+import { useCallback } from "react";
+import { useAnimations } from "../src";
+
+export default { component: useAnimations };
+
+export const Toggle: StoryObj = {
+  render: () => {
+    const animate = useAnimations({
+      red: [{ fill: "red" }, { duration: 600, easing: "ease-out" }],
+      blue: [{ fill: "blue" }, { duration: 600, easing: "ease-out" }],
+      green: [{ fill: "green" }, { duration: 600, easing: "ease-out" }],
+    });
+
+    const onClickRed = useCallback(() => {
+      animate.play("red").end();
+    }, [animate]);
+    const onClickBlue = useCallback(() => {
+      animate.play("blue").end();
+    }, [animate]);
+    const onClickGreen = useCallback(() => {
+      animate.play("green").end();
+    }, [animate]);
+    const onClickAll = useCallback(async () => {
+      await animate.play("red").end();
+      await animate.play("blue").end();
+      await animate.play("green").end();
+    }, [animate]);
+
+    return (
+      <div>
+        <svg width={150} height={150}>
+          <path
+            ref={animate.ref}
+            d="M 10,30 A 20,20 0,0,1 50,30 A 20,20 0,0,1 90,30 Q 90,60 50,90 Q 10,60 10,30 z"
+          />
+        </svg>
+        <div>
+          <button onClick={onClickRed}>Red</button>
+          <button onClick={onClickBlue}>Blue</button>
+          <button onClick={onClickGreen}>Green</button>
+          <button onClick={onClickAll}>All</button>
+        </div>
+      </div>
+    );
+  },
+};
