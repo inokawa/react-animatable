97764f8497a30abe4350185a8c8e4704b864bb84
inokawa <48897392+inokawa@users.noreply.github.com>
2022-11-27 23:53:50 +0900

    Add comments

--- src/react/hooks/useAnimation.ts
+++ src/react/hooks/useAnimation.ts
@@ -26,15 +26,15 @@ export type PlayArgs<Args = void> = Args extends void
 
 export interface BaseAnimationHandle<Args = void> {
   /**
-   * A wrapper of Web Animations API's [play](https://developer.mozilla.org/en-US/docs/Web/API/Animation/play).
+   * A wrapper of Web Animations API's [play](https://developer.mozilla.org/en-US/docs/Web/API/Animation/play). It's similar to GSAP's `play()`.
    */
   play: (...opts: PlayArgs<Args>) => BaseAnimationHandle<Args>;
   /**
-   * A wrapper of Web Animations API's [reverse](https://developer.mozilla.org/en-US/docs/Web/API/Animation/reverse).
+   * A wrapper of Web Animations API's [reverse](https://developer.mozilla.org/en-US/docs/Web/API/Animation/reverse). It's similar to GSAP's `reverse()`.
    */
   reverse: () => BaseAnimationHandle<Args>;
   /**
-   * A wrapper of Web Animations API's [cancel](https://developer.mozilla.org/en-US/docs/Web/API/Animation/cancel).
+   * A wrapper of Web Animations API's [cancel](https://developer.mozilla.org/en-US/docs/Web/API/Animation/cancel). It's similar to GSAP's `kill()`.
    */
   cancel: () => BaseAnimationHandle<Args>;
   /**
@@ -42,11 +42,11 @@ export interface BaseAnimationHandle<Args = void> {
    */
   finish: () => BaseAnimationHandle<Args>;
   /**
-   * A wrapper of Web Animations API's [pause](https://developer.mozilla.org/en-US/docs/Web/API/Animation/pause).
+   * A wrapper of Web Animations API's [pause](https://developer.mozilla.org/en-US/docs/Web/API/Animation/pause). It's similar to GSAP's `pause()`.
    */
   pause: () => BaseAnimationHandle<Args>;
   /**
-   * A setter of Web Animations API's [currentTime](https://developer.mozilla.org/en-US/docs/Web/API/Animation/currentTime).
+   * A setter of Web Animations API's [currentTime](https://developer.mozilla.org/en-US/docs/Web/API/Animation/currentTime). It's similar to GSAP's `seek()`.
    *
    * If you pass function, you can get [endTime](https://developer.mozilla.org/en-US/docs/Web/API/AnimationEffect/getComputedTiming#return_value) from its argument.
    */
@@ -54,7 +54,7 @@ export interface BaseAnimationHandle<Args = void> {
     time: number | ((endTime: number) => number)
   ) => BaseAnimationHandle<Args>;
   /**
-   * A wrapper of Web Animations API's [updatePlaybackRate](https://developer.mozilla.org/en-US/docs/Web/API/Animation/updatePlaybackRate).
+   * A wrapper of Web Animations API's [updatePlaybackRate](https://developer.mozilla.org/en-US/docs/Web/API/Animation/updatePlaybackRate). It's similar to GSAP's `timeScale()`.
    *
    * If you pass function, you can get current [playbackRate](https://developer.mozilla.org/en-US/docs/Web/API/Animation/playbackRate) from its argument.
    */
