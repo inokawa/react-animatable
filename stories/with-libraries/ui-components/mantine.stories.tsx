3753eff693bfe5027fc57a3443827f86042303e5
inokawa <48897392+inokawa@users.noreply.github.com>
2022-11-28 19:57:00 +0900

    Add story for Mantine

---
+++ stories/with-libraries/ui-components/mantine.stories.tsx
@@ -0,0 +1,31 @@
+import { StoryObj } from "@storybook/react";
+import React from "react";
+import { MantineProvider, Button } from "@mantine/core";
+import { useAnimation } from "../../../src";
+
+export default { component: useAnimation };
+
+export const Default: StoryObj = {
+  render: () => {
+    const animate = useAnimation(
+      [{ transform: "rotate(0deg)" }, { transform: "rotate(720deg)" }],
+      {
+        duration: 1000,
+        easing: "ease-in-out",
+      }
+    );
+
+    return (
+      <MantineProvider>
+        <Button
+          ref={animate}
+          onClick={() => {
+            animate.play();
+          }}
+        >
+          Click Me!
+        </Button>
+      </MantineProvider>
+    );
+  },
+};
